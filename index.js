97a4760b6cd92e77df6c4c75c173a2cca098fc6f
Duncan <cheplenginduncan@gmail.com>
2023-03-12 18:10:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = { 
+    name: "Cheplengin",
+    streetAddress: "Ongata Rongai"
+}
+
+function updateEmployeeWithKeyAndValue( employee, key, value) {
+
+    const newEmployee =  { ...employee };
+
+    newEmployee[ key] = value;
+
+    return newEmployee;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[ key] = value;
+
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) { 
+
+    delete {...employee[key]};
+
+    return {...employee[key]};
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    const propToBeDeleted = key;
+
+    delete employee[ propToBeDeleted];
+
+    return employee;
+
+}
+
